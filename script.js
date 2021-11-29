320ce7088c2b7de4bde43427c275a5eccc787311
Elijah Busick <elibusick11@gmail.com>
2021-11-28 20:55:20 -0500

    added end section variables and added the class question structure

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ var quiz_sections = documentary.queryselectorall(".quiz-section");
 //start var
 var start_section = documentary.getElementId("start");
 var start_btn = documentary.getElementId("start-btn");
-//Quiz Section var
+//Quiz section var
 var quiz_section = document.getElementById("quiz-questions");
 var time_remaining = document.getElementById("time-remaining");
 var question = document.getElementById("question");
@@ -11,5 +11,11 @@ var choices = document.getElementById("choices");
 var choice_status = document.querySelectorAll(".choice-status");
 var correct = document.getElementById("correct");
 var wrong = document.getElementById("wrong");
-
+//End section var
+var end_section = document.getElementById("end");
+var end_title = document.getElementById("end-title");
+var score = document.getElementById("score");
+var inital_input = document.getElementById("initials");
+var submit_score = document.getElementById("submit-score");
+var error_message = document.getElementById("error-message");
 
