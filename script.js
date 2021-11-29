fe22c8e1d7838ba0f0465320e2b0380020dd4a85
Elijah Busick <elibusick11@gmail.com>
2021-11-28 20:23:35 -0500

    Added var for start ids and classes

---
+++ script.js
@@ -0,0 +1,9 @@
+//Main section var
+var quiz_sections = documentary.queryselectorall(".quiz-section");
+//start var
+var start_section = documentary.getElementId("start");
+var start_btn = documentary.getElementId("start-btn");
+//Quiz Section var
+
+
+
