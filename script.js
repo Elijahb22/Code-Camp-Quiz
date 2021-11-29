44980afe6434994b92d9781c741389d2003e913a
Elijah Busick <elibusick11@gmail.com>
2021-11-28 20:42:00 -0500

    added var for all quesiton based ids and classes

--- script.js
+++ script.js
@@ -4,6 +4,12 @@ var quiz_sections = documentary.queryselectorall(".quiz-section");
 var start_section = documentary.getElementId("start");
 var start_btn = documentary.getElementId("start-btn");
 //Quiz Section var
-
+var quiz_section = document.getElementById("quiz-questions");
+var time_remaining = document.getElementById("time-remaining");
+var question = document.getElementById("question");
+var choices = document.getElementById("choices");
+var choice_status = document.querySelectorAll(".choice-status");
+var correct = document.getElementById("correct");
+var wrong = document.getElementById("wrong");
 
 
