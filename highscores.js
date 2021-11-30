080a0b4bc81732258d5a0afff06ccf8eb4dd655e
Elijah Busick <elibusick11@gmail.com>
2021-11-29 22:55:05 -0500

    added all functions needed to create a table

--- highscores.js
+++ highscores.js
@@ -1,13 +1,63 @@
-//Highscore vars
+//Highscores var
 var highscore_table = document.getElementById("highscores-table");
 var clear_highscore_btn = document.getElementById("clear-highscores");
-//Event listener to clear highscores
+
+//The event listener
 clear_highscore_btn.addEventListener('click', clearHighscores);
-//This loads the highscore table
+
+//Loads table when page loaded
 generateHighscoresTable();
+
 function generateHighscoresTable() {
   let highscores = localStorage.getItem("scoreList");
   if (highscores) {
     addHighscoreTableRows(highscores);
   } 
+}
+
+//generates highscore to the the table
+function addHighscoreTableRows(highscores) {
+  highscores = JSON.parse(highscores);
+
+  highscores.forEach(function(scoreItem, index) {
+    var rankCell = createRankCell(index + 1);
+    var scoreCell = createScoreCell(scoreItem.score);
+    var initialsCell = createInitialsCell(scoreItem.initials);
+    var highscoreTableRow = createHighscoreTableRow(rankCell, scoreCell, initialsCell);
+    highscoreTableRow.appendChild(highscoreTableRow);
+  });
+}
+
+function createRankCell(rank) {
+  var rankCell = document.createElement('td');
+  rankCell.textContent = `#${rank}`;
+  return rankCell;
+}
+
+function createScoreCell(score) {
+  var scoreCell = document.createElement('td');
+  scoreCell.textContent = score;
+  return scoreCell;
+}
+
+function createInitialsCell(initials) {
+  var initialsCell = document.createElement('td');
+  initialsCell.textContent = initials;
+  return initialsCell;
+}
+
+function createHighscoreTableRow(rankCell, scoreCell, initialsCell) {
+  var tableRow = document.createElement('tr');
+  tableRow.appendChild(rankCell);
+  tableRow.appendChild(scoreCell);
+  tableRow.appendChild(initialsCell);
+  return tableRow;
+}
+
+//Clears the table
+function clearHighscores() {
+  localStorage.setItem('scoreList', []);
+  while (highscore_table.children.length > 1) {
+    highscore_table.removeChild(highscore_table.lastChild);
+  }
 }
